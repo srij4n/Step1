03127f6b98836f53fa2e8a4c2d54dd7154a088e5
srij4n <outsidesrijan@gmail.com>
2026-02-21 18:14:25 +0530

    UC2: Added comment to banner code

--- src/OOPSBannerUC2.java
+++ src/OOPSBannerUC2.java
@@ -1,3 +1,8 @@
+/**
+ * UC1 - Print "OOPS" to console
+ * @author Srijan
+ * @version 1.0
+ */
 public class OOPSBannerUC2 {
     public static void main(String[] args) {
         System.out.println("   ***   " + "   ***   " + " ******  " + "  ****** ");
